90625fb269df6dd003a0715aefaa9dfec16f49e9
EmanVB <emanbonsato@yahoo.com>
2024-03-09 16:32:45 +0800

    Update js files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+// Additional styles import
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((imgs) => {
+
+    let li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${imgs.original}">
+            <img
+                class="gallery__image"
+                src="${imgs.preview}"
+                alt="${imgs.description}"
+            />
+        </a>
+    </li>
+    `;
+    gallery.innerHTML += li;
+});
+
+
+const resultImg = new SimpleLightbox(".gallery a", {captionsData: "alt", captionDelay: 250});
+resultImg.on("show.simplelightbox");    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form.feedback-form');
+const emailBox = document.querySelector('label [name="email"]');
+const messageBox = document.querySelector('label [name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  const email = emailBox.value;
+  const message = messageBox.value;
+
+  const formData = {
+    email,
+    message,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+onPageReload();
+
+function onPageReload() {
+  const savedInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedInput) {
+    emailBox.value = savedInput.email;
+    messageBox.value = savedInput.message;
+  }
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = emailBox.value;
+  const message = messageBox.value;
+
+  if (email == '' || message == '') {
+    alert('Please complete the form inputs!');
+    form.reset();
+    return;
+  }
+
+  const formData = { email, message };
+  console.log(formData);
+  form.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+}
